af3176ac48dd96ed678d2740bcc1b2ce0d103902
Marina Kalashnikova <malvinabluehair256@gmail.com>
2021-03-05 07:47:16 +0000

    Add an option, that makes my default pascal project

--- template.sh
+++ template.sh
@@ -6,10 +6,12 @@ do
     case $2 in
     "py")
         echo -ne "#! /usr/bin/python3\n" > $name
+        chmod 754 $name
     ;;
 
     "sh")
         echo -ne "#! /bin/bash\n" > $name
+        chmod 754 $name
     ;;
 
     "desktop")
@@ -23,10 +25,18 @@ Exec=
 Categories=
 Category=\
 " > $name
+    chmod 754 $name
+    ;;
+
+    "pas")
+        mkdir $1; cd $1
+        cat /home/$USER/Documents/git/BASH/.nibba > B.sh
+        chmod 754 B.sh
+        mkdir src
+        touch notes.txt src/main.pas
     ;;
 
     esac
-    chmod 754 $name
 
     shift
     shift
